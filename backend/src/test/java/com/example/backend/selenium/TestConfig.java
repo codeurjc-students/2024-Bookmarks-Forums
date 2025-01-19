68e16b4a8d68faafc8ecefa8d3cb7e1cb936b4c9
Etheko. <i.ruizba.2021@alumnos.urjc.es>
2025-01-19 02:00:43 +0100

    Fixed waitTime timeout

--- backend/src/test/java/com/example/backend/selenium/TestConfig.java
+++ backend/src/test/java/com/example/backend/selenium/TestConfig.java
@@ -25,7 +25,7 @@ public class TestConfig { // Singleton pattern for testing config params.
     private TestConfig() {
         this.localhost = "http://localhost";
         this.port = 4200; // Default port
-        this.waitTime = 0;
+        this.waitTime = 20;
 
         this.username = "BookReader_14";
         this.password = "pass";
